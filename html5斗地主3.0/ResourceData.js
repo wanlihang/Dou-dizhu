8ea3dbb545fc6acef0fc8563318cafcf353110a4
wanlihang <344051580@qq.com>
2019-01-03 07:55:17 +0800

    Add files via upload

---
+++ html5斗地主3.0/ResourceData.js
@@ -0,0 +1,97 @@
+﻿
+
+var ResourceData= {
+    Images:{
+        //path：文件相对路径，picNum:当前图片中宽和高包含的小图片数，cellSize:小图片的宽和高，data：存放加载的图片数据。
+        //goddess_new:{path:"images/goddess_new.png", picNum:{WNum:4,HNum:4},cellSize:{width:32,height:48}, data:null}
+        1:{path:"img/1.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:18}
+        ,2:{path:"img/2.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:17}
+        ,3:{path:"img/3.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:3}
+        ,4:{path:"img/4.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:4}
+        ,5:{path:"img/5.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:5}
+        ,6:{path:"img/6.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:6}
+        ,7:{path:"img/7.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:7}
+        ,8:{path:"img/8.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:8}
+        ,9:{path:"img/9.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:9}
+        ,10:{path:"img/10.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:10}
+        ,11:{path:"img/11.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:11}
+        ,12:{path:"img/12.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:12}
+        ,13:{path:"img/13.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:13}
+        ,14:{path:"img/14.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:14}
+        ,15:{path:"img/15.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:4,num:16}
+        ,16:{path:"img/16.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:3}
+        ,17:{path:"img/17.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:4}
+        ,18:{path:"img/18.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:5}
+        ,19:{path:"img/19.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:6}
+        ,20:{path:"img/20.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:7}
+        ,21:{path:"img/21.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:8}
+        ,22:{path:"img/22.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:9}
+        ,23:{path:"img/23.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:10}
+        ,24:{path:"img/24.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:11}
+        ,25:{path:"img/25.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:12}
+        ,26:{path:"img/26.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:13}
+        ,27:{path:"img/27.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:14}
+        ,28:{path:"img/28.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:3,num:16}
+        ,29:{path:"img/29.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:3}
+        ,30:{path:"img/30.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:4}
+        ,31:{path:"img/31.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:5}
+        ,32:{path:"img/32.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:6}
+        ,33:{path:"img/33.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:7}
+        ,34:{path:"img/34.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:8}
+        ,35:{path:"img/35.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:9}
+        ,36:{path:"img/36.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:10}
+        ,37:{path:"img/37.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:11}
+        ,38:{path:"img/38.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:12}
+        ,39:{path:"img/39.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:13}
+        ,40:{path:"img/40.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:14}
+        ,41:{path:"img/41.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:2,num:16}
+        ,42:{path:"img/42.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:3}
+        ,43:{path:"img/43.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:4}
+        ,44:{path:"img/44.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:5}
+        ,45:{path:"img/45.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:6}
+        ,46:{path:"img/46.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:7}
+        ,47:{path:"img/47.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:8}
+        ,48:{path:"img/48.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:9}
+        ,49:{path:"img/49.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:10}
+        ,50:{path:"img/50.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:11}
+        ,51:{path:"img/51.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:12}
+        ,52:{path:"img/52.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:13}
+        ,53:{path:"img/53.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:14}
+        ,54:{path:"img/54.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:168}, data:null,se:1,num:16}
+        ,BeiMian:{path:"img/beimian.png", picNum:{WNum:1,HNum:1},cellSize:{width:97,height:137}, data:null}
+        ,startbg:{path:"img/startbg.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:1280,height:720}, data:null}
+		,btn:{path:"img/btn.png", picNum:{WNum:1,HNum:1},cellSize:{width:128,height:44}, data:null}
+        ,buchu:{path:"img/buchu.png", picNum:{WNum:1,HNum:1},cellSize:{width:55,height:28}, data:null}
+		,chupai:{path:"img/chupai.png", picNum:{WNum:1,HNum:1},cellSize:{width:56,height:30}, data:null}
+		,tishi:{path:"img/tishi.png", picNum:{WNum:1,HNum:1},cellSize:{width:56,height:30}, data:null}
+        ,bg1:{path:"img/playbg.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:700,height:525}, data:null}
+        ,startbtn:{path:"img/start.png", picNum:{WNum:1,HNum:1},cellSize:{width:288,height:103}, data:null}
+        ,clickstartbtn:{path:"img/clickstart.png", picNum:{WNum:1,HNum:1},cellSize:{width:288,height:103}, data:null}
+        ,setbtn:{path:"img/shezhi.png", picNum:{WNum:1,HNum:1},cellSize:{width:56,height:68}, data:null}
+        ,setp:{path:"img/set.png", picNum:{WNum:1,HNum:1},cellSize:{width:218,height:216}, data:null}
+        ,music:{path:"img/music.png", picNum:{WNum:1,HNum:1},cellSize:{width:81,height:92}, data:null}
+        ,clomusic:{path:"img/clomusic.png", picNum:{WNum:1,HNum:1},cellSize:{width:85,height:95}, data:null}
+        ,closebtn:{path:"img/close.png", picNum:{WNum:1,HNum:1},cellSize:{width:76,height:76}, data:null}
+        ,endbg:{path:"img/end.png", picNum:{WNum:1,HNum:1},cellSize:{width:592,height:567}, data:null}
+        ,successbg:{path:"img/success.png", picNum:{WNum:1,HNum:1},cellSize:{width:156,height:114}, data:null}
+        ,failedbg:{path:"img/failed.png", picNum:{WNum:1,HNum:1},cellSize:{width:156,height:114}, data:null}
+        ,exit:{path:"img/exit.png", picNum:{WNum:1,HNum:1},cellSize:{width:187,height:83}, data:null}
+        ,clickexit:{path:"img/cliexit.png", picNum:{WNum:1,HNum:1},cellSize:{width:187,height:83}, data:null}
+        ,again:{path:"img/again.png", picNum:{WNum:1,HNum:1},cellSize:{width:156,height:69}, data:null}
+        ,clickagain:{path:"img/cliagain.png", picNum:{WNum:1,HNum:1},cellSize:{width:156,height:69}, data:null}
+        ,qd:{path:"img/queding.png", picNum:{WNum:1,HNum:1},cellSize:{width:156,height:69}, data:null}
+        ,clickqd:{path:"img/cliqueding.png", picNum:{WNum:1,HNum:1},cellSize:{width:187,height:83}, data:null}
+
+
+
+    },
+    Sound:{
+        //soundName:声音文件名称，path：文件夹相对路径，data：存放加载的声音数据。由于各种浏览器对声音格式的支持不一致，声音文件格式有MP3和OGG两种
+        //sound01:{soundName:"sound01",path:"sound/", data:null,num:17}
+    },
+    Animation:{
+         //ZS1_move:{imageName:"goddess_new",beginPoint:{WNum:0,HNum:0},allPlayNum:4,times:2}
+    }
+
+
+};
---
+++ html5斗地主3.0/gif.js
@@ -0,0 +1,32 @@
+function diufanqie()
+    {
+    	document.getElementById("fq").style.zIndex=100;
+    }
+    function diufanqie2()
+    {
+    	document.getElementById("fq2").style.zIndex=100;
+    }
+    function qx()
+    {
+    	document.getElementById("fq").style.zIndex=-1;
+    }
+    function qx2()
+    {
+    	document.getElementById("fq2").style.zIndex=-1;
+    }
+    function daocha()
+    {
+    	document.getElementById("tea").style.zIndex=100;
+    }
+    function daocha2()
+    {
+    	document.getElementById("tea2").style.zIndex=100;
+    }
+    function qx3()
+    {
+    	document.getElementById("tea").style.zIndex=-1;
+    }
+    function qx4()
+    {
+    	document.getElementById("tea2").style.zIndex=-1;
+    }
